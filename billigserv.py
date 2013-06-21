e64e071bb34ebf921b6013e991696e45a0cd9a76
flxw <felix@flxw.de>
2013-06-22 00:08:00 +0200

    First working request, fulfills basic requirements

--- billigserv.py
+++ billigserv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import socket
+import os
 
 s = socket.socket()
 host = socket.gethostname()
@@ -8,9 +9,30 @@ port = 8080
 s.bind((host, port))
 s.listen(5)
 
-print("billig HTTP-Server jetzt live auf ", host, ':' port)
+print("billiger HTTP-Server jetzt live auf ", host, ':', port)
 
 while True:
     conn, clientaddr = s.accept()
-    print('Got connection from ', clientaddr)
+    print('Verbindung erhalten von: ', clientaddr)
+
+    # handle connection request here
+    commandInfoList = str(conn.recv(4096), 'utf-8').split('\n',1)[0].split(' ')
+    fileLocation = commandInfoList[1]
+    httpCommand = commandInfoList[0]
+
+    del commandInfoList
+
+    if httpCommand == "GET":
+        if fileLocation == "/":
+            fileLocation = os.getcwd() + "/index.html"
+        else:
+            fileLocation = os.getcwd() + fileLocation
+    
+        inFile = open(fileLocation, 'rt')
+        fileBytes = inFile.read().encode("utf-8")
+        inFile.close()
+
+        conn.send(fileBytes)
+
+    # finally close connection
     conn.close()
