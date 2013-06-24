464ff35dccf3a1f044cdda60e668bda0efac16df
flxw <felix@flxw.de>
2013-06-24 21:45:45 +0200

    Reorganized some more and found partial send bug

--- getsome.py
+++ getsome.py
@@ -5,23 +5,23 @@ import os
 import time
 import threading
 
-def buildLocationString(reqPath):
+def buildLocationStringAndStatus(reqPath):
     if reqPath == "/":
         reqPath = os.getcwd() + "/index.html"
     else:
         reqPath = os.getcwd() + reqPath
 
-    if os.path.exists(reqPath):
-        return reqPath
+    if os.path.isfile(reqPath):
+        return (reqPath, 200)
     else:
-        return None
+        return (os.getcwd() + "/404.html", 404)
 
 def buildResponseHeader(status):
     responseDict = { 200 : "HTTP/1.1 200 OK",
-                   400 : "HTTP/1.1 400 Bad Request",
-                   404 : "HTTP/1.1 404 File not found" }
+                     400 : "HTTP/1.1 400 Bad Request",
+                     404 : "HTTP/1.1 404 Not Found" }
 
-    header  =  responseDict[statusCode] + "\n"
+    header  =  responseDict[status] + "\n"
     header  += time.strftime("Date: %a, %d %b %Y %H:%M:%S GMT\n")
     header  += "Content-Type: text/html\n"
     header  += "Content-Encoding: UTF-8\n"
@@ -34,10 +34,12 @@ def sendResponse(sinkSocket, statusCode, responseContent=""):
        statusCode = integer with HTTP response code
        responseContent = string that contains the response body"""
 
-    responseHeader = buildResponseHeader(statusCode)
-    
-    sinkSocket.send(responseHeader.encode("utf-8"))
-    sinkSocket.send(responseContent.encode("utf-8"))
+    response = buildResponseHeader(statusCode) + responseContent
+
+    totalsent = 0
+    while totalsent < len(response):
+        totalsent += sinkSocket.send(response[totalsent:].encode('utf-8'))
+        print(totalsent, " bytes sent!")
 
     # finally close connection
     sinkSocket.close()
@@ -60,25 +62,20 @@ def handleRequestInThread(clsock):
     body       = "";
 
     if httpCommand == "GET":
-        fileLocation = buildLocationString(fileLocation)
+        fileLocation, statusCode = buildLocationStringAndStatus(fileLocation)
 
-        if fileLocation != None:
-            inFile = open(fileLocation, 'rt')
-            body = inFile.read()
-            statusCode    = 200;
-            inFile.close()
-        else:
-            statusCode = 404;
+        inFile     = open(fileLocation, 'rt')
+        body       = inFile.read()
+        inFile.close()
     else:
         statusCode = 400;
 
     sendResponse(clsock, statusCode, body)
 
-
 if __name__ == '__main__':
     s = socket.socket()
     host = socket.gethostname()
-    port = 8888
+    port = 8080
     s.bind((host, port))
     s.listen(5)
 
@@ -89,4 +86,5 @@ if __name__ == '__main__':
 
         t = threading.Thread(target=handleRequestInThread, args=[conn])
         t.start()
+
         print('Handling request from ', clientaddr, ' in thread with ID \'', t.name, '\'')
